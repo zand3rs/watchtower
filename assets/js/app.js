1a73b6bebefd09814fcbdf4ee59ae6e8acb1dced
Leslie <resurii14@gmail.com>
2015-08-08 16:57:41 +0800

    UI: Removed graph for now

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,5 @@
 // ----------------------------------------------------------------------------
 $(function() {
 
-  // Dashboard
-  // ---------------------------------------------------------------------------
-  if ($("#dashboard-graph").length && dashboardGraphData) {
-    // Polar Area graph for log types
-    var ctx = $("#dashboard-graph")[0].getContext("2d");
-    var dashboardChart = new Chart(ctx).PolarArea(dashboardGraphData);
-  }
 
 });
